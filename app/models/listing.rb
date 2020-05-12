7f52ea666ddb9a39b07f7dbe88f53520cca4333d
Aubyn92 <livstaubyn92@gmail.com>
2020-05-12 12:39:44 +1000

    Created _form.html.erb for listings and gave each listing a user_id in the schema file before adding active storage

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,8 @@
 class Listing < ApplicationRecord
     belongs_to :user
     belongs_to :category
+    has_one_attached :picture
+    validates :title, presence: true
+    has_many :cart_listings
+    has_many :carts, through: :cart_listings
 end
