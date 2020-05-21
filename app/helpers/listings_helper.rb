db0a82bff5fe06d42c63bc6943c8fb6d30759505
Aubyn92 <livstaubyn92@gmail.com>
2020-05-21 10:47:01 +1000

    Fixed error in the helper file and listings view for uploading images correctly

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,20 +1,9 @@
-# module ListingsHelper
-#     def picture_for(listing)
-#         return "
-#           background: url(#{url_for(listing.picture)});
-#           height: 100%; 
-#           background-position: center; 
-#           background-size: cover;
-#         "
-#     end    
-# end
-
 module ListingsHelper
   def picture_for(listing)
     if listing.picture.present?
       listing.picture
     else
-      'red-pop.jpg'
+      asset_path 'red-pop.jpg'
     end
   end
 end
